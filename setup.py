8519ce5c95f2315868880f9041850821927cb6cd
Adam Greene <github@mzpqnxow.com>
2023-09-07 10:48:22 -0400

    add explicit bounds on supported python versions

--- setup.py
+++ setup.py
@@ -13,9 +13,17 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/sarenehan/cache_to_disk",
     packages=setuptools.find_packages(),
-    classifiers=(
+    requires_python=">3.4,<=3.11",
+    classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ),
+    ],
 )
